49e105941b5794e3ffaf9786cfddc412cc7806dc
milaseptiyori <62972989+milaseptiyori@users.noreply.github.com>
2023-03-06 18:30:07 +0700

    first commit

---
+++ application/views/admin/laporan/v_lap_pembelian.php
@@ -0,0 +1,115 @@
+<html lang="en" moznomarginboxes mozdisallowselectionprint>
+<head>
+    <title>Laporan data pembelian</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/laporan.css')?>"/>
+</head>
+<body onload="window.print()">
+<div id="laporan">
+<table align="center" style="width:900px; border-bottom:3px double;border-top:none;border-right:none;border-left:none;margin-top:5px;margin-bottom:20px;">
+<!--<tr>
+    <td><img src="<?php// echo base_url().'assets/img/kop_surat.png'?>"/></td>
+</tr>-->
+</table>
+
+<table border="0" align="center" style="width:800px; border:none;margin-top:5px;margin-bottom:0px;">
+<tr>
+    <td colspan="2" style="width:800px;paddin-left:20px;"><center><h4>LAPORAN PEMBELIAN BARANG</h4></center><br/></td>
+</tr>
+                       
+</table>
+ 
+<table border="0" align="center" style="width:900px;border:none;">
+        <tr>
+            <th style="text-align:left"></th>
+        </tr>
+</table>
+
+<table border="1" align="center" style="width:900px;margin-bottom:20px;">
+<thead>
+    <tr>
+        <th style="width:50px;">No</th>
+        <th>No Faktur</th>
+        <th>Tanggal</th>
+        <th>Kode Barang</th>
+        <th>Nama Barang</th>
+        <th>Satuan</th>
+        <th>Harga Pokok</th>
+        <th>Harga Jual</th>
+        <th>Qty</th>
+        <th>Diskon</th>
+        <th>Total</th>
+    </tr>
+</thead>
+<tbody>
+<?php 
+$no=0;
+    foreach ($data->result_array() as $i) {
+        $no++;
+        $nofak=$i['beli_nofak'];
+        $tgl=$i['beli_tanggal'];
+        $barang_id=$i['d_beli_barang_id'];
+        $barang_nama=$i['d_beli_barang_nama'];
+        $barang_satuan=$i['d_beli_barang_satuan'];
+        $barang_harjul=$i['d_beli_barang_harga'];
+        $barang_qty=$i['d_beli_qty'];
+        $barang_diskon=$i['d_beli_diskon'];
+        $barang_total=$i['d_beli_total'];
+?>
+    <tr>
+        <td style="text-align:center;"><?php echo $no;?></td>
+        <td style="padding-left:5px;"><?php echo $nofak;?></td>
+        <td style="text-align:center;"><?php echo $tgl;?></td>
+        <td style="text-align:center;"><?php echo $barang_id;?></td>
+        <td style="text-align:left;"><?php echo $barang_nama;?></td>
+        <td style="text-align:left;"><?php echo $barang_satuan;?></td>
+        <td style="text-align:right;"><?php echo 'Rp '.number_format($barang_harga);?></td>
+        <td style="text-align:center;"><?php echo $barang_qty;?></td>
+        <td style="text-align:right;"><?php echo 'Rp '.number_format($barang_diskon);?></td>
+        <td style="text-align:right;"><?php echo 'Rp '.number_format($barang_total);?></td>
+    </tr>
+<?php }?>
+</tbody>
+<tfoot>
+<?php 
+    $b=$jml->row_array();
+?>
+    <tr>
+        <td colspan="9" style="text-align:center;"><b>Total</b></td>
+        <td style="text-align:right;"><b><?php echo 'Rp '.number_format($b['total']);?></b></td>
+    </tr>
+</tfoot>
+</table>
+<table align="center" style="width:800px; border:none;margin-top:5px;margin-bottom:20px;">
+    <tr>
+        <td></td>
+</table>
+<table align="center" style="width:800px; border:none;margin-top:5px;margin-bottom:20px;">
+    <tr>
+        <td align="right">Padang, <?php echo date('d-M-Y')?></td>
+    </tr>
+    <tr>
+        <td align="right"></td>
+    </tr>
+   
+    <tr>
+    <td><br/><br/><br/><br/></td>
+    </tr>    
+    <tr>
+        <td align="right">( <?php echo $this->session->userdata('nama');?> )</td>
+    </tr>
+    <tr>
+        <td align="center"></td>
+    </tr>
+</table>
+<table align="center" style="width:800px; border:none;margin-top:5px;margin-bottom:20px;">
+    <tr>
+        <th><br/><br/></th>
+    </tr>
+    <tr>
+        <th align="left"></th>
+    </tr>
+</table>
+</div>
+</body>
+</html>_
